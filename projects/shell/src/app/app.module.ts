28a14c3f1ea2a58a3a66c8bfc93bdf17920bb61d
Yathartha <varshneya.yathartha@gmail.com>
2023-08-14 18:37:00 +0530

    2023-08-14T18:37:00+05:30

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { ProductsComponent } from './products/products.component';
-// import { ProductComponent } from './products/product.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ProductsComponent,
-    // ProductComponent
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
