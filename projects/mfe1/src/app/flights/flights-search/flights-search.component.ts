2ad5cfc5b6796dbc384a90bbeaf5bac33f5f839e
Yathartha <varshneya.yathartha@gmail.com>
2023-09-10 22:06:37 +0530

    Resolved issues

--- projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
+++ projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -6,24 +6,10 @@ import {Component, ViewChild, ViewContainerRef, Inject, Injector, ComponentFacto
   templateUrl: './flights-search.component.html'
 })
 export class FlightsSearchComponent {
-
-  @ViewChild('vc', { read: ViewContainerRef, static: true })
-  viewContainer: ViewContainerRef;
-
-  constructor(
-    @Inject(Injector) private injector,
-    @Inject(ComponentFactoryResolver) private cfr) { }
+  constructor() { }
 
   search() {
     alert('Not implemented for this demo!');
   }
-
-  async terms() {
-    const comp = await import('../lazy/lazy.component').then(m => m.LazyComponent);
-
-    const factory = this.cfr.resolveComponentFactory(comp);
-    this.viewContainer.createComponent(factory, null, this.injector);
-  }
-
 
 }
--- projects/mfe2/src/app/bookings/bookings-search/bookings-search.component.ts
+++ projects/mfe2/src/app/bookings/bookings-search/bookings-search.component.ts
@@ -5,24 +5,9 @@ import {Component, ViewChild, ViewContainerRef, Inject, Injector, ComponentFacto
   templateUrl: './bookings-search.component.html'
 })
 export class BookingsSearchComponent {
-
-  @ViewChild('vc', { read: ViewContainerRef, static: true })
-  viewContainer: ViewContainerRef;
-
-  constructor(
-    @Inject(Injector) private injector,
-    @Inject(ComponentFactoryResolver) private cfr) { }
+  constructor() { }
 
   search() {
     alert('Not implemented for this demo!');
   }
-
-  async terms() {
-    const comp = await import('../lazy/lazy.component').then(m => m.LazyComponent);
-
-    const factory = this.cfr.resolveComponentFactory(comp);
-    this.viewContainer.createComponent(factory, null, this.injector);
-  }
-
-
 }
